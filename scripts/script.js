88a0330e2d2c4dff8a19120e3915f1a47d5b0f83
Osman Zakir <osmanzakir90@hotmail.com>
2020-01-29 17:01:19 +0500

    Edited comment

--- scripts/script.js
+++ scripts/script.js
@@ -5,5 +5,5 @@ test --> ..  <!----> ..
 `;
 
 // Find all matches of zero or more of any character between comment tags, having "any character" include
-// mulitple lines
+// newlines
 alert(str.match(/<!--.*?-->/gs));
